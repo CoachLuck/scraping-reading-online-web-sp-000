93b733a093561b255724db92d2bf68c70d81d6a3
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:03:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,5 +8,4 @@ courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 courses.each { |course|
   p "Course: #{course.children[1].children[0].text}"
   p "Description: #{course.children[1].children[2].children[0].children[0].text}"
-  p ""
 }

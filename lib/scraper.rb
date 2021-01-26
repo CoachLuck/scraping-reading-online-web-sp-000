c722085361afd4526ff86f8d0761a183da2bf636
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:01:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,5 +7,5 @@ doc = Nokogiri::HTML(html)
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 courses.each { |course|
   p "Course: #{course.children[1].children[0].text}"
-  p "Description: #{course.children[1].children[2].text}\n"
+  p "Description: #{course.children[1].children[2].text} + \n"
 }
